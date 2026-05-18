1448597e0e165a0d68fc324764caa97b80dce1d5
Anthony Ricardo <147872743+anthony-ricardox@users.noreply.github.com>
2026-05-18 02:53:54 -0300

    utilizando document.Element

---
+++ getElemnt.js
@@ -0,0 +1,13 @@
+
+const animais = document.getElementById('animais')
+console.log(animais)
+
+const gridSection = document.getElementsByClassName('grid-section')
+
+console.log(gridSection)
+
+const primeiroLi = document.querySelector('li')
+console.log(primeiroLi)
+
+const primeiroUl = document.querySelector('ul')
+console.log(primeiroUl.innerText)
