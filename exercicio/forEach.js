f177682c2bf92b44ebad38dfb5ac22a2c8289c6a
Anthony Ricardo <147872743+anthony-ricardox@users.noreply.github.com>
2026-05-18 03:58:13 -0300

    exercicio de metodo forEach e desafio concluido

---
+++ exercicio/forEach.js
@@ -0,0 +1,22 @@
+/*const imgs = document.querySelectorAll("img");
+
+imgs.forEach(function (item, index, array) {
+  //console.log(item, index, array);
+});*/
+
+const titulos = document.getElementsByClassName("titulo");
+
+const titulosArray = Array.from(titulos);
+
+titulosArray.forEach(function (item, index, array) {
+  //console.log(item, index, array);
+});
+
+
+const imgs = document.querySelectorAll("img");
+
+imgs.forEach((item) => {
+   console.log(item);
+});
+
+
