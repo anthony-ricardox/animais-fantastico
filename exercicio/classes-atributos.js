782e266159e8d657e3dcef182926269b6400b140
Anthony Ricardo <147872743+anthony-ricardox@users.noreply.github.com>
2026-05-31 18:11:27 -0300

    getAttribute e setAttribute Métodos que retornam ou definem de acordo com o atributo selecionado

--- exercicio/classes-atributos.js
+++ exercicio/classes-atributos.js
@@ -1,15 +1,27 @@
-const menu = document.querySelector('.menu')
+const menu = document.querySelector(".menu");
 
-menu.classList.add('ativo', 'amor')
-menu.classList.remove('ativo')
-menu.classList.replace('amor', 'miramor')
+menu.classList.add("ativo", "amor");
+menu.classList.remove("ativo");
+menu.classList.replace("amor", "miramor");
 
-if(menu.classList.contains('vida')){
-    menu.classList.add('mozaum')
-}else{
-    menu.classList.add('naoPossui-Vida')
+if (menu.classList.contains("vida")) {
+  menu.classList.add("mozaum");
+} else {
+  menu.classList.add("naoPossui-Vida");
 }
 
-menu.className = '13'
+menu.className += " name";
 
-console.log(menu.className)
+//console.log(menu.className)
+
+const animais = document.querySelector(".animais");
+
+console.log(animais.attributes);
+
+const img = document.querySelector("img");
+const srcImg = img.getAttribute("src");
+img.setAttribute("alt", "amor");
+
+const possuiAlt = img.hasAttribute('title')
+
+console.log(possuiAlt);
