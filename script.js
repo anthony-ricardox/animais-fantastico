64cd280e15eb1ba2bc4efcee39dcb5525d67cd78
Anthony Ricardo <147872743+anthony-ricardox@users.noreply.github.com>
2026-06-28 17:31:32 -0300

    feat: função com loop para cada index ele ativa e remover a class active, não fica ativa dois conteudo, apenas um

---
+++ script.js
@@ -0,0 +1,11 @@
+const tabMenu = document.querySelectorAll(".js-tabmenu li");
+
+const tabContent = document.querySelectorAll(".js-tabcontent section");
+
+function activeTab(index){   
+    tabContent.forEach((section)=>{    
+        section.classList.remove('active')
+    })
+    tabContent[index].classList.add('active')
+
+}
